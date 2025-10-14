fbdca2561da98e00daa8ef8e246b2f08abf99b6a
joshi-07 <sughoshak6@gmail.com>
2025-10-14 16:55:11 +0530

    ADD new feature

--- script.js
+++ script.js
@@ -6,3 +6,17 @@ function selectYear(year) {
 function goToSubjects(branch) {
   window.location.href = `subjects/${branch}.html`;
 }
+
+function selectSubject(subject) {
+  const path = window.location.pathname;
+  const branch = path.split('/').pop().split('.')[0];
+  localStorage.setItem('selectedSubject', subject);
+  localStorage.setItem('selectedBranch', branch);
+  window.location.href = `../options/options_${branch}.html`;
+}
+
+function selectOption(option) {
+  const branch = localStorage.getItem('selectedBranch');
+  const subject = localStorage.getItem('selectedSubject');
+  window.location.href = `${branch}_${option}.html`;
+}
